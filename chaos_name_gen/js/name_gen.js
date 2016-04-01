5823b490059bc60e4be454b4abce5f8361a81925
David Varner <david.r.varner@gmail.com>
2016-04-01 14:38:54 -0400

    Working from JSON file

--- chaos_name_gen/js/name_gen.js
+++ chaos_name_gen/js/name_gen.js
@@ -1,59 +1,68 @@
 $(document).ready(function() {
 
+   $.getJSON('data/data.min.json', function(data) {
+
+      data = data['names'];
+      for ( var i = 0; i < data.length; i++ ) {
+         names.push(data[i]);
+      }
+
+   });
+
    var championName = '';
+   var names = [];
 
    $('#generate').click(function(e) {
       e.preventDefault();
 
+      var first = '';
+      var second = '';
+      var title = '';
       var god = $('input[name="god"]:checked').val() - 1;
-      var firstName = generateName(aos, 'first', rollDice());
-      console.log("firstName: " + firstName);
-      var secondName = generateName(aos, 'second', rollDice());
-      console.log("secondName: " + secondName);
-      var title = generateTitle(aos, god, rollDice());
-      console.log("title: " + title);
 
-      championName = firstName + secondName + ' ' + title;
+      first = generateName(names, 0, rollDice());
+      second = generateName(names, 1, rollDice());
+      title = generateTitle(names, god, rollDice());
 
-      $('#display-name').html("<h2>"+championName+"</h2>");
+      championName = first + second + ' ' + title;
 
+      $('#display-name').html('<h2>' + championName + '</h2>');
    });
 
+   function randomRange(max) {
+      number = Math.floor((Math.random() * max) + 1);
+
+      return number;
+   }
+
+   function rollDice() {
+      d1 = randomRange(4).toString();
+      d2 = randomRange(6).toString();
+
+      return d1 + d2;
+   }
+
+   function generateName(data, pos, roll) {
+      var name = '';
+
+      data.forEach(function(entry) {
+         if (entry['roll'] === roll) {
+            name = entry['data'][pos];
+         }
+      });
+
+      return name;
+   }
+
+   function generateTitle(data, god, roll) {
+      var title = '';
+
+      data.forEach(function(entry) {
+         if (entry['roll'] === roll) {
+            title = entry['data'][2][god];
+         }
+      });
+
+      return title;
+   }
 });
-
-function randomRange(max) {
-   number = Math.floor((Math.random() * max) + 1);
-   return number;
-}
-
-function rollDice() {
-   d1 = randomRange(6).toString();
-   d2 = randomRange(6).toString();
-
-   console.log("Roll: " + (d1 + d2));
-   return d1 + d2;
-}
-
-function generateName(data, pos, roll) {
-   var name = '';
-
-   data.forEach(function(entry) {
-      if (entry['roll'] === roll) {
-         name = entry[pos];
-      }
-   });
-
-   return name;
-}
-
-function generateTitle(data, god, roll) {
-   var title = '';
-
-   data.forEach(function(entry) {
-      if (entry['roll'] === roll) {
-         title = entry['title'][god];
-      }
-   });
-
-   return title;
-}

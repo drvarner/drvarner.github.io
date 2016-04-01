6af8e42f46b62c03f2ed57c177fd16f546d53a26
David Varner <david.r.varner@gmail.com>
2016-04-01 12:38:58 -0400

    Converted data.js to JSON and minified

--- chaos_name_gen/data/data.js
+++ chaos_name_gen/data/data.js
@@ -1,422 +1,453 @@
-var aos = [
     {
-        "roll": "11",
-        "first": "Ska",
-        "second": "ilax",
-        "title":
-        [
+        "11": [
+        "Ska",
+        "ilax",
+                [
             "The Thrice-damned",
             "the Blooded",
             "Fatewhisper",
             "the Pestilent",
             "Paingiver"
         ]
-    },
+
+]    },
     {
-        "roll": "12",
-        "first": "Drok",
-        "second": "toth",
-        "title":
-        [
+        "12": [
+        "Drok",
+        "toth",
+                [
             "the Reviled",
             "Bloodborn",
             "Warpkin",
             "the Maggot King",
             "the Tormentor"
         ]
-    },
+
+]    },
     {
-        "roll": "13",
-        "first": "Fel",
-        "second": "gul",
-        "title":
-        [
+        "13": [
+        "Fel",
+        "gul",
+                [
             "the Desolator",
             "the Enraged",
             "the All-seeing",
             "Poxbringer",
             "the Salacious"
         ]
-    },
+
+]    },
     {
-        "roll": "14",
-        "first": "Luc",
-        "second": "on",
-        "title": [
+        "14": [
+        "Luc",
+        "on",
+        [
             "Fellheart",
             "Skullbreaker",
             "Flameborn",
             "Plague Bringer",
             "Soulshriver"]
     },
-    {
-        "roll": "15",
-        "first": "Hur",
-        "second": "zahr",
-        "title": [
+
+]    {
+        "15": [
+        "Hur",
+        "zahr",
+        [
             "Worldbreaker",
             "Rageheart",
             "the Changed",
             "the Corpulent",
             "the Eternal"]
     },
-    {
-        "roll": "16",
-        "first": "Val",
-        "second": "kar",
-        "title": [
+
+]    {
+        "16": [
+        "Val",
+        "kar",
+        [
             "the Cursed",
             "Brassheart",
             "the Mutator of Worlds",
             "Blubbermaw",
             "Silverquick"]
     },
-    {
-        "roll": "21",
-        "first": "Am",
-        "second": "ros",
-        "title": [
+
+]    {
+        "21": [
+        "Am",
+        "ros",
+        [
             "Blacksteel",
             "Bloodscream",
             "Warplord",
             "Rot-soul",
             "Pain-lord"]
     },
-    {
-        "roll": "22",
-        "first": "Rut",
-        "second": "la",
-        "title": [
+
+]    {
+        "22": [
+        "Rut",
+        "la",
+        [
             "Blacksoul",
             "the Decapitator",
             "Fate-lord",
             "Filthmonger",
             "Painspawned"]
     },
-    {
-        "roll": "23",
-        "first": "Sor",
-        "second": "kia",
-        "title": [
+
+]    {
+        "23": [
+        "Sor",
+        "kia",
+        [
             "Gibbermaw",
             "the Slayer",
             "the Destined",
             "Festerheart",
             "Bladeslash"]
     },
-    {
-        "roll": "24",
-        "first": "Sig",
-        "second": "nar",
-        "title": [
+
+]    {
+        "24": [
+        "Sig",
+        "nar",
+        [
             "the Blessed",
             "Skulleater",
             "Warp-born",
             "Flyborn",
             "Thrashblood"]
     },
-    {
-        "roll": "25",
-        "first": "Baud",
-        "second": "baal",
-        "title": [
+
+]    {
+        "25": [
+        "Baud",
+        "baal",
+        [
             "Twiceborn",
             "Head-taker",
             "Windblade",
             "the Infested",
             "the Magnificent"]
     },
-    {
-        "roll": "26",
-        "first": "Eg",
-        "second": "wulf",
-        "title": [
+
+]    {
+        "26": [
+        "Eg",
+        "wulf",
+        [
             "Neverborn",
             "Blood-spiller",
             "the Twisted",
             "the Infectious",
             "the Depraved"]
     },
-    {
-        "roll": "31",
-        "first": "Mor",
-        "second": "zel",
-        "title": [
+
+]    {
+        "31": [
+        "Mor",
+        "zel",
+        [
             "the Destroyer",
             "Gore-lord",
             "Warpmaw",
             "Grimblech",
             "the Flayer"]
     },
-    {
-        "roll": "32",
-        "first": "Ab",
-        "second": "nir",
-        "title": [
+
+]    {
+        "32": [
+        "Ab",
+        "nir",
+        [
             "Soulchewer",
             "Flesh-render",
             "Fluxlord",
             "Bileheart",
             "the Corrupt"]
     },
-    {
-        "roll": "33",
-        "first": "Aek",
-        "second": "am",
-        "title": [
+
+]    {
+        "33": [
+        "Aek",
+        "am",
+        [
             "Flesh Gorger",
             "Blood Hunter",
             "the Schemer",
             "Poxbringer",
             "the Debauched"]
     },
-    {
-        "roll": "34",
-        "first": "Gul",
-        "second": "vald",
-        "title": [
+
+]    {
+        "34": [
+        "Gul",
+        "vald",
+        [
             "Vilespawn",
             "Bloodlord",
             "the Unknowing",
             "Plague Bringer",
             "Limbcleaver"]
     },
-    {
-        "roll": "35",
-        "first": "Dech",
-        "second": "rar",
-        "title": [
+
+]    {
+        "35": [
+        "Dech",
+        "rar",
+        [
             "Deathbringer",
             "the Everwrathful",
             "the Architect",
             "the Corpulent",
             "the Denied"]
     },
-    {
-        "roll": "36",
-        "first": "Kat",
-        "second": "rimm",
-        "title": [
+
+]    {
+        "36": [
+        "Kat",
+        "rimm",
+        [
             "Warbringer",
             "Redmaw",
             "Balefire",
             "Blubbermaw",
             "Thrice-tainted"]
     },
-    {
-      "roll": "41",
-      "first": "Scy",
-      "second": "drek",
-      "title": [
+
+]    {
+      "41": [
+      "Scy",
+      "drek",
+      [
          "the Chosen",
          "Redaxe",
          "the Incandescent",
          "Rot-soul",
          "Daggerheart"
       ]
+   ]
    },
    {
-      "roll": "42",
-      "first": "Khaz",
-      "second": "ax",
-      "title": [
+      "42": [
+      "Khaz",
+      "ax",
+      [
          "the Unholy",
          "Skull-lord",
          "Warpspawned",
          "Filthmonger",
          "the Pallid"
       ]
+   ]
    },
    {
-      "roll": "43",
-      "first": "Gorth",
-      "second": "dol",
-      "title": [
+      "43": [
+      "Gorth",
+      "dol",
+      [
          "the Undefeated",
          "the Rampager",
          "the Deceiver",
          "Phlegmchild",
          "Helbound"
       ]
+   ]
    },
    {
-      "roll": "44",
-      "first": "Kran",
-      "second": "lac",
-      "title": [
+      "44": [
+      "Kran",
+      "lac",
+      [
          "the Reaper",
          "Gorebringer",
          "the Ancient One",
          "the Decayed",
          "the Seeker"
       ]
+   ]
    },
    {
-      "roll": "45",
-      "first": "Kithel",
-      "second": "bar",
-      "title": [
+      "45": [
+      "Kithel",
+      "bar",
+      [
          "the Malefic",
          "Lord of Brass",
          "the Ascendant",
          "the Bloated",
          "Helheart"
       ]
+   ]
    },
    {
-      "roll": "46",
-      "first": "Hel",
-      "second": "nath",
-      "title": [
+      "46": [
+      "Hel",
+      "nath",
+      [
          "Scourgeborn",
          "the Berserker",
          "the Acolyte",
          "Soulblight",
          "Lord of Despair"
       ]
+   ]
    },
    {
-      "roll": "51",
-      "first": "Char",
-      "second": "har",
-      "title": [
+      "51": [
+      "Char",
+      "har",
+      [
          "the Slaughterer",
          "Lord of Skulls",
          "Doomkindred",
          "the Fecund",
          "Lord of Tortured Souls"
       ]
+   ]
    },
    {
-      "roll": "52",
-      "first": "Barg",
-      "second": "nalax",
-      "title": [
+      "52": [
+      "Barg",
+      "nalax",
+      [
          "Doomlord",
          "Gorethirster",
          "Lord of Fate",
          "Lord of Blights",
          "the Defiler"
       ]
+   ]
    },
    {
-      "roll": "53",
-      "first": "Vash",
-      "second": "tragar",
-      "title": [
+      "53": [
+      "Vash",
+      "tragar",
+      [
          "the Despised",
          "Bloodspawned",
          "the Infernal",
          "Plaguespawned",
          "Painfeaster"
       ]
+   ]
    },
    {
-      "roll": "54",
-      "first": "Bub",
-      "second": "asor",
-      "title": [
+      "54": [
+      "Bub",
+      "asor",
+      [
          "Lord of Ruin",
          "Lord of Blood",
          "the Aetherking",
          "the Necrotic",
          "the Despoiler Lord"
       ]
+   ]
    },
    {
-      "roll": "55",
-      "first": "Loth",
-      "second": "lixus",
-      "title": [
+      "55": [
+      "Loth",
+      "lixus",
+      [
          "the Reaver",
          "Fleshtearer",
          "the Eternal Watcher",
          "the Festering",
          "Whipperslash"
       ]
+   ]
    },
    {
-      "roll": "56",
-      "first": "Freg",
-      "second": "rak",
-      "title": [
+      "56": [
+      "Freg",
+      "rak",
+      [
          "Scion of Chaos",
          "Bloodblade",
          "Lord of Aeons",
          "the Scabrous One",
          "Painsworn"
       ]
+   ]
    },
    {
-      "roll": "61",
-      "first": "Dec",
-      "second": "us",
-      "title": [
+      "61": [
+      "Dec",
+      "us",
+      [
          "the Everdamned",
          "Skullsworn",
          "Fireblade",
          "Stench Lord",
          "Manflayer"
       ]
+   ]
    },
    {
-      "roll": "62",
-      "first": "Tor",
-      "second": "gan",
-      "title": [
+      "62": [
+      "Tor",
+      "gan",
+      [
          "Feaster of Souls",
          "Bloodroar",
          "the Changer",
          "Blubberblade",
          "Lord of Blades"
       ]
+   ]
    },
    {
-      "roll": "63",
-      "first": "Zhrakk",
-      "second": "gald",
-      "title": [
+      "63": [
+      "Zhrakk",
+      "gald",
+      [
          "Daemonblood",
          "Skulltaker",
          "the Sorceror King",
          "Poxmaw",
          "Darkblade"
       ]
+   ]
    },
    {
-      "roll": "64",
-      "first": "Dar",
-      "second": "vor",
-      "title": [
+      "64": [
+      "Dar",
+      "vor",
+      [
          "Warpblade",
          "Blood-drinker",
          "Mageblade",
          "the Putrid",
          "Scion of Pain"
       ]
+   ]
    },
    {
-      "roll": "65",
-      "first": "Fes",
-      "second": "ash",
-      "title": [
+      "65": [
+      "Fes",
+      "ash",
+      [
          "the Possessed",
          "Brassblade",
          "Lord of Sorcery",
          "Lord of Buboes",
          "Lord of Torment"
       ]
+   ]
    },
    {
-      "roll": "66",
-      "first": "Ark",
-      "second": "on",
-      "title": [
+      "66": [
+      "Ark",
+      "on",
+      [
          "Lord of Chaos",
          "Goresplitter",
          "the Insane",
          "Rotblade",
          "the Corruptor"
       ]
+   ]
    }
-]
